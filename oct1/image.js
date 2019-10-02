796d304b3e7f943a29e4d129a1bbd0ea065054d6
becskeens <55206376+becskeens@users.noreply.github.com>
2019-10-02 14:42:32 -0500

    Update image.js

--- oct1/image.js
+++ oct1/image.js
@@ -1,7 +1,7 @@
 var img;
 
 function preload(){
-    img=loadImage("https://becskeens.github.io/Skeens_Bec_ART2210/raw/master/imageLink/img/motorcycletire.jpg.jpg");
+    img=loadImage("https://becskeens.github.io/Skeens_Bec_ART2210/raw/master/oct1/motorcycletire.jpg.jpg");
     
 }
 
